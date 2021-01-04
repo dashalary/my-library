594743a1f75307d0ae9176d9d1e138e1649d963c
Dasha Lary <dasha4988@gmail.com>
2021-01-04 14:03:59 -0500

    edited form details for the book model

--- my-library-frontend/src/index.js
+++ my-library-frontend/src/index.js
@@ -1,5 +1,22 @@
 const apiService = new ApiService()
 let main = document.getElementById('main')
 
-
+function displayCreateForm() {
+    let formDiv = document.querySelector("#new-book-form")
+    let html = `
+    <form>
+    <label>Title: </label>
+    <input type="text" id="title">
+    <label>Author: </label>
+    <input type="text" id="author">
+    <label>Author: </label>
+    <input type="text" id="author">
+    <label>Have you already read it? </label>
+    <input type="checkbox" id="read">
+    <input type="submit">
+    </form>
+    `
+    formDiv.innerHTML = html 
+    document.querySelector('form').addEventListener('submit', addBook)
+}
 
